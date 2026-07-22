4dfcb285270e6025dce6c6334e0f4834bd30ed13
Eugene <evgenijgoncarevic@gmail.com>
2026-07-22 20:30:41 +0300

    upgrade: help text upgrade

--- handlers/_help.py
+++ handlers/_help.py
@@ -2,11 +2,14 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
+import bot
+
 router = Router()
 
 
 @router.message(Command("help"))
 async def help(message: Message):
+    bot_info = await bot.Bot.get_me(bot.bot)
     text = (
         ""
         "Введите запрос для поиска трека.\n"
@@ -15,7 +18,8 @@ async def help(message: Message):
         "[music.youtube.com](https://music.youtube.com/) (это обязательно, никакие другие ссылки не сработают)"
         " - введите команду "
         "`/force` и через пробел вставьте *ссылку* на видео.\n"
-        "Пример: `/force https://music.youtube.com/watch?v=qdCU4ReBROc`"
+        "```Пример: /force https://music.youtube.com/watch?v=qdCU4ReBROc```"
+        f"\nДоступен inline-режим. Напишите в любом чате @{bot_info.username or 'username_бота'} <трек>"
     )
 
     return message.answer(text, parse_mode="Markdown")
