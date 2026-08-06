ae40ccfc80f0964a692c566a22fe0862c8d14a14
Eugene <evgenijgoncarevic@gmail.com>
2026-08-06 19:57:43 +0300

    feat: get ytm playlist link func

--- helpers/get_ytm_video_link.py
+++ helpers/get_ytm_video_link.py
@@ -1,2 +1,6 @@
 def get_ytm_video_link(video_id: str):
     return f"https://music.youtube.com/watch?v={video_id}"
+
+
+def get_ytm_playlist_link(playlist_id: str):
+    return f"https://music.youtube.com/playlist?list={playlist_id}"
