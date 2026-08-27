1743b5513b336af0c76ff24cff12c8dae9527a4e
Eugene <evgenijgoncarevic@gmail.com>
2026-08-27 14:49:45 +0300

    refactor: worker OOM optimization
    involved lock to prevent too many create_task calls

--- worker/new_worker.py
+++ worker/new_worker.py
@@ -34,6 +34,7 @@ class WorkerPipe:
         self._active_downloads: dict[str, list[F]] = {}
         self._pool_semaphore = asyncio.Semaphore(WORKER_CORES_COUNT)
         self._send_semaphore = asyncio.Semaphore(2)
+        self._create_task_semaphore = asyncio.Semaphore(30)
         self._disk_space_semaphore = asyncio.Semaphore(30)
         self._worker_task: asyncio.Task | None = None
 
@@ -130,6 +131,8 @@ class WorkerPipe:
                 LOGGER.debug(f"Setting result to worker tasks on {v_id}")
                 fut.set_result((v_id, DownloadResult(file_id, is_too_large, is_error)))
 
+        return self._create_task_semaphore.release()
+
     async def _send_non_cached_to_telegram(
         self, cache: TrackDirContentDict, *, title: str, artist: str
     ):
@@ -153,7 +156,7 @@ class WorkerPipe:
                     performer=artist,
                     request_timeout=300,
                 )
-                LOGGER.info(f"Send track {title} to channel")
+                LOGGER.info(f"Sent track {title} to channel")
                 break
 
             except TelegramNetworkError as e:
@@ -186,6 +189,7 @@ class WorkerPipe:
         )
 
         while True:
+            await self._create_task_semaphore.acquire()
             if self._queue.empty() and self._q_shift:
                 self._reset_shift()
             task = await self._queue.get()
